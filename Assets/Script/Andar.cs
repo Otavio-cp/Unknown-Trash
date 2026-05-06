5f217c44b66ad35d557d3ef1b2585ec308c239e5
Otavio-cp <otavio61284596@alunos.sc.senac.br>
2026-05-06 14:37:50 -0300

    Mecanica de andar

---
+++ Assets/Script/Andar.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Andar : MonoBehaviour
+{
+    public int vel = 5;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Vector3 Move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            Move.y = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Move.y = -1;
+        }
+
+
+
+
+
+
+
+        transform.position += Move.normalized * vel * Time.deltaTime;
+    }
+}
