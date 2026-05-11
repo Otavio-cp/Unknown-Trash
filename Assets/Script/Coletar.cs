c10e56befd5433e0999a02618cc696fb2863b056
Otavio-cp <otavio61284596@alunos.sc.senac.br>
2026-05-11 16:39:19 -0300

    Adição de tag ao sumir o obj e correção no codigo

--- Assets/Script/Coletar.cs
+++ Assets/Script/Coletar.cs
@@ -18,11 +18,17 @@ public class Coletar : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    void OnTriggerEnter(Collider other)
     {
         
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
-
 }
+
+
 
 
