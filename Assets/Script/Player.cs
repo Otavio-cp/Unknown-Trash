f54593d4b4b4086ca5c9ae26fc0aa85fdafa6153
Otavio-cp <otavio61284596@alunos.sc.senac.br>
2026-09-10 11:38:16 -0300

    Add victory bar to Player and recovered scene
    Expose a victory/progress Slider in Player.cs (Barradevida) with MaximoParaGanhar and TamanhoBarra fields. Initialize the slider max value on start and update its value as the player grows (value = current scale - 1) so player size is reflected as progress toward win. Also add a recovered Unity scene file Assets/_Recovery/0 (4).unity and its .meta.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -35,7 +35,12 @@ public class Player : MonoBehaviour
     [SerializeField] float tamanhoAumentar2 = 25f;
     [SerializeField] float tamanhoDimi = 6f;
 
-    
+    [Header("Barra de vitoria")]
+    [SerializeField] Slider Barradevida;
+    [SerializeField] float MaximoParaGanhar = 24f;
+    [SerializeField] float TamanhoBarra;
+
+
 
     public static bool Natelaupgrades = false;
 
@@ -43,6 +48,7 @@ public class Player : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         Screen.fullScreen = true;
+        Barradevida.highValue = MaximoParaGanhar;
     }
     
 
@@ -92,6 +98,8 @@ public class Player : MonoBehaviour
             {
                 scal += aumentodetamanho;
                 transform.localScale = new Vector3(scal, scal, scal);
+                TamanhoBarra = transform.localScale.x;
+                Barradevida.value = TamanhoBarra - 1;
                 itensParaAumentar = 0;
             }
 
