14af33f9dd354880a8da241e721e5b2974200941
migbarros <migbarros2011@gmail.com>
2026-09-24 16:36:19 -0300

    quase terinindo

---
+++ Assets/Cainos/Pixel Art Platformer - Village Props/Script/Editor/ChestEditor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using Cainos.Common;
+
+namespace Cainos.PixelArtPlatformer_VillageProps
+{
+    [CustomEditor(typeof(Chest))]
+    public class ChestEditor : InspectorEditor
+    {
+    }
+}
---
+++ Assets/Cainos/Pixel Art Platformer - Village Props/Script/Editor/ElevatorEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using Cainos.Common;
+
+
+namespace Cainos.PixelArtPlatformer_VillageProps
+{
+    [CustomEditor(typeof(Elevator))]
+    public class ElevatorEditor : InspectorEditor
+    {
+    }
+}
